f005cebf4bd4df7af9e08d57db27797cafa691f9
Maxence Pellerin <maxence.pellerin@epitech.eu>
2022-05-22 01:22:45 +0200

    boh

--- CurrencyRate.js
+++ CurrencyRate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         [Pokeclicker] Currency rate
+// @name         CurrencyRate
 // @namespace    Pokeclicker Scripts
-// @version      0.5
+// @version      0.3
 // @description  Display curency rate
 // @author       Maxteke
 // @match        https://www.pokeclicker.com/
